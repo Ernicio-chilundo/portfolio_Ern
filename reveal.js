ec1633eedfcf7217436aa5536487c39afcbff2f9
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-09 14:11:08 +0200

    feito

--- reveal.js
+++ reveal.js
@@ -59,3 +59,66 @@ mostrar.reveal('.scroll_reveal-sobre',
     delay: 1500,
     origin: 'right'
 })
+
+// SESSAO PORTFOLIO
+mostrar.reveal('.scroll_reveal-portfolio',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'bottom'
+})
+mostrar.reveal('.scroll_reveal-portfolio1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'bottom'
+})
+mostrar.reveal('.scroll_reveal-portfolio2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'bottom'
+})
+
+// SESSAO FORMUARIO
+ mostrar.reveal('.scroll_reveal-formulario',
+{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom'
+})
+
+mostrar.reveal(' .scroll_reveal-formulario1',
+{
+    duration: 3000,
+    distance: '90px',
+    delay: 1200,
+    origin: 'bottom'
+})
+
+mostrar.reveal(' .scroll_reveal-formulario2',
+{
+    duration: 3000,
+    distance: '90px',
+    delay: 1800,
+    origin: 'bottom'
+})
+
+mostrar.reveal(' .scroll_reveal-formulario3',
+{
+    duration: 3000,
+    distance: '90px',
+    delay: 2400,
+    origin: 'bottom'
+})
+mostrar.reveal(' .scroll_reveal-formulario',
+{
+    duration: 3000,
+    distance: '90px',
+    delay: 3000,
+    origin: 'bottom'
+})
+
