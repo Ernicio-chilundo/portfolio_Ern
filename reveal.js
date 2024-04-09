f847c4e1b3eba6b3f9552a742de5a58c9ee39260
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-04-09 13:00:03 +0200

    usado a biblioteca ScrollReveal no meu portfolio

---
+++ reveal.js
@@ -0,0 +1,61 @@
+window.mostrar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+mostrar.reveal('.scroll_reveal-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+mostrar.reveal('.scroll_reveal-img',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// SESSAO DOS TITULOS
+mostrar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+// SESSAO DAS ESPECIALIDADES
+mostrar.reveal('.efeito_especialidade',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+mostrar.reveal('.efeito_especialidade1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+mostrar.reveal('.efeito_especialidade2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// SESSAO SOBRE
+mostrar.reveal('.scroll_img-reveal',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1000,
+    origin: 'left'
+})
+
+mostrar.reveal('.scroll_reveal-sobre',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1500,
+    origin: 'right'
+})
